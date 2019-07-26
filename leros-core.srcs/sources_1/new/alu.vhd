0216e9e6e10e0150f8a71fe80558a36e54d78f8e
Morten Borup Petersen <morten_bp@live.dk>
2019-07-26 15:45:56 +0200

    Modify srl to sra

--- leros-core.srcs/sources_1/new/alu.vhd
+++ leros-core.srcs/sources_1/new/alu.vhd
@@ -24,7 +24,7 @@ begin
         res <=  (others => '0') when nop,
                 op1 + op2 when add,
                 op1 - op2 when sub,
-                op1 srl 1 when shra,
+                op1(op1'left) & op1(op1'left downto 1) when shra, -- sra 1
                 op1 and op2 when alu_and,
                 op1 or op2 when alu_or,
                 op1 xor op2 when alu_xor,
