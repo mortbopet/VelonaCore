f9948d9726d917da1c644c55e02dc79ef85a1478
Morten Borup Petersen <morten_bp@live.dk>
2019-07-26 13:08:57 +0200

    Initial testbenches commit

---
+++ leros-core.srcs/sim_1/new/loop_tb.vhd
@@ -0,0 +1,48 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity loop_tb is
+end loop_tb;
+
+use work.Common.all;
+
+architecture Behavioral of loop_tb is
+
+    signal clk : std_logic;
+    constant clk_period : time := 1 ns;
+
+    signal pc : unsigned(REG_WIDTH - 1 downto 0);
+    signal instr : std_logic_vector(INSTR_WIDTH - 1 downto 0);
+
+begin
+
+    Core_ent :  entity work.Leros_core
+    port map (
+        pc_sig => pc,
+        dm_data_in => (others => '0'),
+        clk => clk,
+        rst => '0',
+        instr => instr
+    );
+
+    Instr_mem : entity work.ROM
+    generic map (
+        data_width => INSTR_WIDTH,
+        addr_width => 4,
+        init_file => "loop_data.txt"
+    )
+    port map (
+        addr => pc(3 downto 0),
+        data_out => instr
+    );
+    
+    clk_process :process
+    begin
+        clk <= '0';
+        wait for clk_period/2;
+        clk <= '1';
+        wait for clk_period/2;
+    end process;
+
+end Behavioral;
