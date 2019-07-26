fa3b54af5d7a33145b11e02a3e4c134e245b8143
Morten Borup Petersen <morten_bp@live.dk>
2019-07-26 15:24:20 +0200

    Refactor ROM initialization functcions

--- leros-core.srcs/sources_1/new/ROM.vhd
+++ leros-core.srcs/sources_1/new/ROM.vhd
@@ -5,7 +5,7 @@ use std.textio.all;
 
 entity ROM is
     generic (
-        init_file : string := "empty.txt";
+        init_file : string := "";
         data_width  : integer;
         addr_width : integer
     );
@@ -19,26 +19,33 @@ architecture Behavioral of ROM is
     type mem_arr is array((2**addr_width) downto 0) of std_logic_vector(data_width - 1 downto 0);
 
     --Read from file function--
-    impure function initRom (RomFileName : in string) return mem_arr is                            
-    file RomFile : text is in RomFileName;                                                              
-    variable RomFileLine : line;               
-    variable linedata : bit_vector(data_width - 1 downto 0);                                                   
-    variable outrom : mem_arr;                                                                            
-                                                                                                   
-    begin             
-        if RomFileName = "empty.txt" then
-            outrom := (others => (others => '0'));
-        else                                                                       
-            for i in outrom'range loop                                                                        
-                readline (RomFile, RomFileLine);
-                read(RomFileLine, linedata);      
-                outrom(2**addr_width - i) := to_stdlogicvector(linedata);                                             
-            end loop;                                  
-        end if;                                              
-        return outrom;                                                                                         
+    impure function initRom (fn : in string) return mem_arr is
+    file RomFile : text is in fn;
+    variable RomFileLine : line;
+    variable linedata : bit_vector(data_width - 1 downto 0);
+    variable outrom : mem_arr;
+
+    begin
+        for i in outrom'range loop
+            readline (RomFile, RomFileLine);
+            read(RomFileLine, linedata);
+            outrom(2**addr_width - i) := to_stdlogicvector(linedata);
+        end loop;
+        return outrom;
     end function;
 
-    signal mem : mem_arr := initRom(init_file);
+    impure function initRomPrimer(fn : in string) return mem_arr is
+    variable outrom : mem_arr;
+    begin
+        if fn = "" then
+            outrom := (others => (others => '0'));
+        else
+            outrom := initRom(fn);
+        end if;
+        return outrom;
+    end function;
+
+    signal mem : mem_arr := initRomPrimer(init_file);
 
 begin
 
