beec2facb780c1304c266ec3a1701fdf5e919ab0
Morten Borup Petersen <morten_bp@live.dk>
2019-07-27 15:00:33 +0200

    Add script for building and converting C programs

---
+++ testprograms/buildTestProgram.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+###
+# This script will build a C source file using the Leros toolchain,
+# link it with the leros.ld linker script (outputting the .text
+# at address 0x0), and convert it to a text file suitable for loading
+# into a ROM, and place it in the source files folder
+###
+
+
+# $1 = Leros toolchain bin folder
+# $2 = program to build
+
+LINKER_SCRIPT="leros.ld"
+
+ELFFILE=${2}.out
+BINFILE=${2}.bin
+DEST=../leros-core.srcs/sources_1/new/
+
+# Compile
+$1/clang -target leros32 -Xlinker $LINKER_SCRIPT $2 -o $ELFFILE
+
+# Extract binary
+$1/llvm-objcopy -O binary $ELFFILE $BINFILE 
+
+# Convert binary to text
+python bintotxt.py $BINFILE
+
+# Move to source folder
+mv $2.txt $DEST
+
+# Cleanup
+rm $BINFILE
+rm $ELFFILE
